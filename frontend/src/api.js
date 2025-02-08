ada24379fdc13a18d36679732223735a1c76be9c
AMIT YADAV <91722003+DevAmitYadav@users.noreply.github.com>
2025-02-08 16:22:58 +0530

    Update api.js
    Backend Render configure to Frontend Vercel

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/students/"; // Update if needed
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://qpaix-student-management.onrender.com/api/students";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
